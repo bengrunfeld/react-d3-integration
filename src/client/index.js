49969711f173c230ffd265e6998f6125e8a03bd7
bengrunfeld <benjamin.grunfeld@gmail.com>
2017-04-29 14:01:30 -0700

    Activate React Router

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,42 @@
 // Libraries
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, Route, Link } from 'react-router-dom'
 import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux'
 
+
+class Root extends React.Component {
+  render(){
+    return (
+      <h1>Root</h1>
+    )
+  }
+}
+
+class Ben extends React.Component {
+  render(){
+    return (
+      <h1>Hello Ben</h1>
+    )
+  }
+}
+
+class Alex extends React.Component {
+  render(){
+    return (
+      <h1>Hello Alex</h1>
+    )
+  }
+}
+
 render(
-  <div>
-    <h2>Hello Alex</h2>
-  </div>,
+  <HashRouter>
+    <div>
+      <Route path='/ben' component={Ben} />
+      <Route path='/alex' component={Alex} />
+      <Route exact path='/' component={Root} />
+    </div>
+  </HashRouter>,
   document.getElementById('main')
 )
